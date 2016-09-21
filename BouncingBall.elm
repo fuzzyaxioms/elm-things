0fa8293620ceea3af7851681635513e3fb924b12
Daniel <you@example.com>
2016-09-21 01:50:42 -0400

    added fps counter

--- BouncingBall.elm
+++ BouncingBall.elm
@@ -4,7 +4,7 @@ import Html exposing (Html)
 import Html.App as App
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
-import Time exposing (Time, second)
+import Time exposing (Time)
 import Date exposing (Date)
 import Task
 import String
@@ -30,6 +30,9 @@ type alias Model =
     , ballY : Int
     , ballVelocityX : Int
     , ballVelocityY : Int
+    , numFrames : Int
+    , numMS : Time
+    , fps : Float
     }
 
 
@@ -41,20 +44,24 @@ init =
      , ballX=200
      , ballY=100
      , ballVelocityX=10
-     , ballVelocityY=10 }
+     , ballVelocityY=10
+     , numFrames = 0
+     , numMS = 0
+     , fps = 0
+     }
   , Cmd.none)
 
 
 -- UPDATE
 
 type Msg
-  = Step
+  = Step Time
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Step ->
+    Step diff ->
         let
             newX = model.ballX + model.ballVelocityX
             newY = model.ballY + model.ballVelocityY
@@ -62,8 +69,11 @@ update msg model =
             newVelY = if newY <= model.ballRadius || newY + model.ballRadius >= model.canvasHeight then -model.ballVelocityY else model.ballVelocityY
             newX2 = clamp model.ballRadius (model.canvasWidth - model.ballRadius) newX
             newY2 = clamp model.ballRadius (model.canvasHeight - model.ballRadius) newY
+            newNumFrames = model.numFrames + 1
+            newNumMS = model.numMS + diff
+            (newNumFrames2, newNumMS2, newFPS) = if Time.inSeconds newNumMS >= 1.0 then (0,0, newNumFrames / Time.inSeconds newNumMS) else (newNumFrames, newNumMS, model.fps)
         in
-      ({model | ballX = newX2, ballY = newY2, ballVelocityX = newVelX, ballVelocityY = newVelY},
+      ({model | ballX = newX2, ballY = newY2, ballVelocityX = newVelX, ballVelocityY = newVelY, numFrames=newNumFrames2, numMS=newNumMS2, fps=newFPS},
       Cmd.none)
 
 
@@ -71,13 +81,17 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  AnimationFrame.diffs (always Step)
+  AnimationFrame.diffs Step
 
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
-    svg [ viewBox "0 0 400 400", width "400px" ]
-      [ circle [ cx (toString model.ballX), cy (toString model.ballY), r (toString model.ballRadius), fill "#0B79CE" ] []
+    Html.div [] [
+        svg [ viewBox "0 0 400 400", width "400px" ]
+          [ circle [ cx (toString model.ballX), cy (toString model.ballY), r (toString model.ballRadius), fill "#0B79CE" ] []
+          ]
+      , Html.hr [] []
+      , Html.text  (toString model.fps)
       ]
