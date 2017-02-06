b282e5b9ed836c21f37f2de1276d55bbe97e95eb
Daniel <you@example.com>
2017-02-05 22:32:16 -0500

    cut down back to simple template

--- projects/simplerpg/Main.elm
+++ projects/simplerpg/Main.elm
@@ -1,8 +1,6 @@
 module Main exposing (main)
 
 import Html exposing (Html)
-import Html.Events exposing (onSubmit,onInput,onClick)
-import Html.Attributes as Attr
 import Svg exposing (Svg, Attribute)
 import Svg.Attributes as SAttr
 import Time exposing (Time)
@@ -13,10 +11,6 @@ import String
 import AnimationFrame
 import Random.Pcg as Random exposing (Generator, Seed)
 
-import Collage exposing (Form)
-import Element exposing (Element)
-import Transform
-
 
 main : Program Never Model Msg
 main =
@@ -26,65 +20,6 @@ main =
     , update = update
     , subscriptions = subscriptions
     }
-
-
-colorToString : Color -> String
-colorToString clr = 
-    let 
-        {red,green,blue} = Color.toRgb clr
-    in
-    "rgb(" ++ toString red ++ "," ++ toString green ++ "," ++ toString blue ++ ")" 
-
--- MODEL
-
--- Ball stuff
-type alias Ball =
-    { pos : Vec2
-    , vel : Vec2
-    , r : Float
-    , clr : Color
-    , form : Form -- see if we can speed things up by partially generating the filled circle form
-    , svg : Svg Msg -- see if we can speed things up
-    }
-
-newBall : Float -> Float -> Float -> Float -> Float -> Color -> Ball
-newBall r x y dx dy clr = {pos=vec2 x y, vel=vec2 dx dy, r=r, clr=clr, form=Collage.filled clr <| Collage.circle r, svg=Svg.circle [SAttr.r <| toString r, SAttr.color <| colorToString clr] []}
-
-randColor : Generator Color
-randColor =
-    let
-        r1 = Random.int 0 255
-        r2 = Random.int 0 255
-        r3 = Random.int 0 255
-    in
-    Random.map3 Color.rgb r1 r2 r3
-
-randBall : Model -> Generator Ball
-randBall model =
-    let
-        randR = Random.map toFloat <| Random.int 5 50
-        randX = Random.map toFloat <| Random.int (round model.leftBound) (round model.rightBound)
-        randY = Random.map toFloat <| Random.int (round model.downBound) (round model.upBound)
-        randDx = Random.map toFloat <| Random.int 5 200
-        randDy = Random.map toFloat <| Random.int 5 200
-        andM = flip Random.andMap
-    in
-    Random.constant newBall |> andM randR |> andM randX |> andM randY |> andM randDx |> andM randDy |> andM randColor
-
-
-updateBall : Model -> Time -> Ball -> Ball
-updateBall m dt b =
-    let
-        (x,y) = toTuple <| add b.pos <| scale (Time.inSeconds dt) b.vel
-        (dx,dy) = toTuple b.vel
-        (x2,dx2) = if x - b.r <= m.leftBound then (m.leftBound + b.r, -dx) else if x + b.r >= m.rightBound then (m.rightBound - b.r, -dx) else (x, dx)
-        (y2,dy2) = if y - b.r <= m.downBound then (m.downBound + b.r, -dy) else if y + b.r >= m.upBound then (m.upBound - b.r, -dy) else (y, dy)
-    in
-    {b | pos = vec2 x2 y2, vel = vec2 dx2 dy2}
-
-viewBall : Ball -> Form
-viewBall b =
-    Collage.move (toTuple b.pos) <| b.form
 
 type alias FPSCounter = { fps : Float }
 
@@ -103,66 +38,35 @@ updateFPSCounter {fps} diff =
     in
     {fps = (1.0-w)*fps + w * (1.0 / s)}
 
-type alias Model =
-    { canvasHeight : Int
-    , canvasWidth : Int
-    , leftBound : Float
-    , rightBound : Float
-    , upBound : Float
-    , downBound : Float
-    , balls : List Ball
-    , fpsCounter : FPSCounter
-    , inputNum : Int
+type alias Model = 
+    { fpsCounter : FPSCounter
     }
 
-newModel : Int -> Int -> Model
-newModel w h =
-    { canvasHeight=h
-     , canvasWidth=w
-     , leftBound = 0
-     , rightBound = toFloat w
-     , upBound = toFloat h
-     , downBound = 0
-     , balls = []
-     , fpsCounter = newFPSCounter
-     , inputNum = 20
-     }
+newModel : Model
+newModel =
+    { fpsCounter = newFPSCounter
+    }
 
 initModel : Model
-initModel = newModel 600 600
+initModel = newModel
 
 init : (Model, Cmd Msg)
 init =
   ( initModel
-  , (Random.list 20 (randBall initModel) ) |> Random.generate Construct
+  , Cmd.none
   )
 
 
 -- UPDATE
 
 type Msg
-  = Generate
-  | UpdateNum Int
-  | Construct (List Ball)
-  | Step Time
+  = Step Time
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Generate ->
-        (model, (Random.list model.inputNum (randBall model) ) |> Random.generate Construct)
-    
-    UpdateNum n ->
-        ({model | inputNum = n}, Cmd.none)
-
-    Construct balls ->
-        ({model | balls = balls}, Cmd.none)
-
     Step diff ->
-        let 
-            newBalls = List.map (updateBall model diff) model.balls
-        in
-      ({model | balls = newBalls,
+      ({model |
         fpsCounter = updateFPSCounter model.fpsCounter diff},
       Cmd.none)
 
@@ -176,42 +80,21 @@ subscriptions model =
 
 -- VIEW
 
-viewFPS : Float -> Html Msg
-viewFPS fps =
+viewFPS : FPSCounter -> Html Msg
+viewFPS counter =
     let 
+        fps = getFPS counter
         n = round (fps * 100)
         a = toString (n // 100)
         b = String.padLeft 2 '0' <| toString (n % 100) 
     in
     Html.text <| "FPS: " ++ a ++ "." ++ b 
 
-viewCanvas : Model -> Html Msg
-viewCanvas model =
-    let
-        background = Collage.filled Color.black <| Collage.rect (toFloat model.canvasWidth) (toFloat model.canvasHeight)
-        forms = List.map viewBall model.balls
-    in
-    Element.toHtml <| Collage.collage model.canvasWidth model.canvasHeight (background :: forms)
-
-viewCanvasSvg : Model -> Html Msg
-viewCanvasSvg model =
-    let 
-        viewBall b = Svg.circle [SAttr.r <| toString b.r, SAttr.style <| ("fill:" ++ colorToString b.clr), SAttr.cx <| toString <| getX b.pos, SAttr.cy <| toString <| getY b.pos] []
-        balls = List.map viewBall model.balls
-    in
-    Svg.svg [SAttr.width <| toString model.canvasWidth, SAttr.height <| toString model.canvasHeight, SAttr.viewBox <| "0 0 600 600"] balls
-
 view : Model -> Html Msg
 view model =
     let
-        canvas = viewCanvasSvg model
-        fps = viewFPS <| getFPS model.fpsCounter
-        inputNum = Html.input [onInput (UpdateNum << Result.withDefault 20 << String.toInt)] []
-        resetButton = Html.button [onClick Generate] [Html.text "Generate"]
+        fps = viewFPS model.fpsCounter
     in
-    Html.body [] [
-        canvas
-        , Html.hr [] []
-        , Html.p [] [fps]
-        , Html.p [] [Html.text "Enter number of balls: ", inputNum,resetButton]
+    Html.body []
+    [ Html.p [] [fps]
     ] 
